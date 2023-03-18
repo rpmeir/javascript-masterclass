b845a73a34d886ba12386a24dd077509fe6b955b
Rodrigo Pires Meira <rpmeir@gmail.com>
2023-03-17 21:08:45 -0300

    number and numeric operators

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { run } from './public/02-data-types/03-number'
+import { run } from './public/02-data-types/03-1-numeric-operators'
 
 document.querySelector('#app').innerHTML = `
   <div id="result" >
